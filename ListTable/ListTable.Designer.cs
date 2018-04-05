6cf3dd5c96d1cb37293fbe05f16dca6da831312e
Joseph Rice <jrice@ourstuff.space>
2018-04-05 18:53:56 -0500

    first commit

---
+++ ListTable/ListTable.Designer.cs
@@ -0,0 +1,47 @@
+﻿namespace ListTable
+{
+    partial class ListTable
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // ListTable
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "ListTable";
+            this.Text = "List Table";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
